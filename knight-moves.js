26afc899bd4d1807970d82c4b78604d383a22662
Kris Mendoza <thekittengallery@gmail.com>
2025-03-02 01:13:56 +0800

    Implement #generateMoves() for generating valid moves

--- knight-moves.js
+++ knight-moves.js
@@ -2,6 +2,7 @@ class Chessboard {
   constructor() {
     this.board = new Map();
     this.#initBoard();
+    this.#generateMoves('2,2');
   }
 
   #initBoard() {
@@ -11,10 +12,35 @@ class Chessboard {
       }
     }
   }
+
+  #generateMoves(pos) {
+    const [x, y] = pos.split(',').map(Number);
+    const moveOffsets = [
+      [1, 2],
+      [2, 1],
+      [1, -2],
+      [2, -1],
+      [-2, 1],
+      [-1, 2],
+      [-1, -2],
+      [-2, -1],
+    ];
+    const validMoves = [];
+    moveOffsets.forEach((offset) => {
+      const nextX = offset[0] + x;
+      const nextY = offset[1] + y;
+
+      if (nextX < 0 || nextX > 7 || nextY < 0 || nextY > 7) return;
+      validMoves.push([nextX, nextY]);
+    });
+
+    console.log(validMoves);
+    return validMoves;
+  }
 }
 
 const chessboard = new Chessboard();
 
 console.log(chessboard.board);
 
-const knightMoves = (x, y) => {};
+const knightMoves = (start, end) => {};
